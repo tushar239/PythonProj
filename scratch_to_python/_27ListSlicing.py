b82660e176b111b2645c1f39cacecca73fe8d414
tushar239 <tushar239@yahoo.com>
2022-01-23 19:12:24 +0530

    first

---
+++ scratch_to_python/_27ListSlicing.py
@@ -0,0 +1,6 @@
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+
+print(letters[0])  # A
+print(letters[1:7])  # ['B', 'C', 'D', 'E', 'F', 'G']
+print(letters[-5:-1])  # ['F', 'G', 'H', 'I']
+print(letters[1:7:2])  # ['B', 'D', 'F']
