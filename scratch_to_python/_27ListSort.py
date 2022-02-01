cb17258d39895891eef628120df2ad3aa870809c
tushar239 <tushar239@yahoo.com>
2022-02-01 15:54:01 +0530

    first

---
+++ scratch_to_python/_27ListSort.py
@@ -0,0 +1,8 @@
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)  # [23, 50, 65, 82, 100]
+
+# Sort Descending
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse=True)
+print(thislist)  # ['pineapple', 'orange', 'mango', 'kiwi', 'banana']
