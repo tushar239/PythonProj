afe53cd2660f614d8b411f0dffa85ee560f3afe8
tushar239 <tushar239@yahoo.com>
2022-02-08 12:31:35 +0530

    first

--- scratch_to_python/_58SelectRecords.py
+++ scratch_to_python/_58SelectRecords.py
@@ -21,3 +21,34 @@ for row in cursor:
     # print(type(row))  # Tuple
     print(row)
 
+print("#############################")
+#######################################################################
+# Select only the name and address columns:
+cursor.execute("SELECT name, address FROM customers")
+
+myresult = cursor.fetchall()
+
+for x in myresult:
+    print(x)
+
+print("#############################")
+##############################################################################################
+# fetchone() - If you are only interested in one row, you can use the fetchone() method.
+# When you do this, records are fetched lazily one by one through the connection. If there are records in the cursor which are not fetched and if you try to reuse or close the cursor, database will complain.
+# To stop this happening, you have multiple options, in which putting 'limit' in sql query is the best
+# https://stackoverflow.com/questions/29772337/python-mysql-connector-unread-result-found-when-using-fetchone
+# 1. connection.cursor(buffered=True)
+    # The reason is that without a buffered cursor, the results are "lazily" loaded, meaning that "fetchone" actually only fetches one row from the full result set of the query. When you will use the same cursor again, it will complain that you still have n-1 results (where n is the result set amount) waiting to be fetched. However, when you use a buffered cursor the connector fetches ALL rows behind the scenes and you just take one from the connector so the mysql db won't complain.
+# 2. put 'LIMIT' in sql query
+
+# cursor.execute("SELECT * FROM customers")
+cursor.execute("SELECT * FROM customers LIMIT 0, 1")
+
+myresult = cursor.fetchone()
+
+print(myresult)
+
+print("#############################")
+
+cursor.close()
+connection.close()
