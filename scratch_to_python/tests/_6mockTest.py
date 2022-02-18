172a2bafe1981035e6315199ba4aa7a59c671f38
tushar239 <tushar239@yahoo.com>
2022-02-18 19:19:38 +0530

    first

--- scratch_to_python/tests/_6mockTest.py
+++ scratch_to_python/tests/_6mockTest.py
@@ -1,14 +1,23 @@
 import unittest  # unittest is a packagename
 from unittest import mock, main  # unittest package has module 'mock' and variable 'main'. this variable is assigned a class TestProgram. so, when you do main(), it instantiates TestProgram class.
-from unittest.mock import MagicMock
+from unittest.mock import Mock, MagicMock
 
-
+"""
+There are two ways of creating a mock.
+- Using MagicMock or Mock (https://www.programcreek.com/python/example/81554/unittest.mock.MagicMock)
+    MagicMock is derived from Mock only
+- Using @patch  (https://www.geeksforgeeks.org/python-unit-test-objects-patching-set-1/)
+Both are same.
+"""
 class TestMocking(unittest.TestCase):
 
     def testGetName(self):
+        # mocked_b = Mock()
+        # or
         mocked_b = MagicMock()
         mocked_b.getname.return_value = "John"
-        a = A(mocked_b)
+
+        a = A(mocked_b)  # passing mocked object
 
         self.assertEqual("John, how are you?", a.getname())
         print(mocked_b.getname.assert_called)
