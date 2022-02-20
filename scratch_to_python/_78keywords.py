c4203f1f583bdb1e9a7f1a77d19cab9186783935
tushar239 <tushar239@yahoo.com>
2022-02-20 17:17:06 +0530

    first

---
+++ scratch_to_python/_78keywords.py
@@ -0,0 +1,16 @@
+"""
+https://www.geeksforgeeks.org/python-keywords/?ref=lbp
+"""
+# Python code to demonstrate working of iskeyword()
+
+# importing "keyword" for keyword operations
+import keyword
+
+# printing all keywords at once using "kwlist()"
+print("The list of keywords is : ")
+print(keyword.kwlist)
+"""
+[‘False’, ‘None’, ‘True’, ‘and’, ‘as’, ‘assert’, ‘async’, ‘await’, ‘break’, ‘class’, ‘continue’, 
+‘def’, ‘del’, ‘elif’, ‘else’, ‘except’, ‘finally’, ‘for’, ‘from’, ‘global’, ‘if’, ‘import’, ‘in’, ‘is’, 
+‘lambda’, ‘nonlocal’, ‘not’, ‘or’, ‘pass’, ‘raise’, ‘return’, ‘try’, ‘while’, ‘with’, ‘yield’]
+"""
---
+++ scratch_to_python/_79ReturnAndYield.py
@@ -0,0 +1,53 @@
+"""
+https://www.geeksforgeeks.org/use-yield-keyword-instead-return-keyword-python/
+
+The yield statement suspends function’s execution and sends a value back to the caller, but retains enough state to enable function to resume where it is left off. When resumed, the function continues execution immediately after the last yield run.
+This allows its code to produce a series of values over time, rather than computing them at once and sending them back like a list.
+"""
+
+# A generator function that yields 1 for the first time,
+# 2 second time and 3 third time
+def simpleGeneratorFun():
+    print("hi 1")
+    yield 1
+    print("hi 2")
+    yield 2
+    print("hi 3")
+    yield 3
+    print("hi 4")
+
+# Driver code to check above generator function
+v = simpleGeneratorFun()
+print(v)  # <generator object simpleGeneratorFun at 0x000002B878215EE0>
+for value in v:
+    print(value)
+
+print("hi")
+
+
+# Return keyword
+def fun():
+    S = 0
+
+    for i in range(10):
+        S += i
+    return S
+
+
+print(fun())
+
+
+# Yield Keyword
+def fun():
+    S = 0
+
+    for i in range(10):
+        S += i
+        yield S
+
+
+v = fun()
+print(v)
+print()
+for i in v:
+    print(i, end=", ")
