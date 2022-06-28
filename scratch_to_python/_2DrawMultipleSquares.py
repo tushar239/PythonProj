9e8383a794c8d4c8f83e2b551e2b13e7e1bc9db5
tushar239 <tushar239@yahoo.com>
2022-06-27 18:50:18 -0700

    Initial Commit

--- scratch_to_python/_2DrawMultipleSquares.py
+++ scratch_to_python/_2DrawMultipleSquares.py
@@ -1,6 +1,7 @@
 # Teach this code without using function and loops. Teach this code again after teaching 'Controls'.
 
 import turtle
+import time
 
 # introducing a function
 def drawsquare():
@@ -29,6 +30,7 @@ turtle.setpos(0, 0)
 
 for num in range(0, 4):  # this is like for(int i=0; i<4; i++) in java
     drawsquare()
+    time.sleep(1)
     # To draw a second square, turn left by 90 degrees
     turtle.left(90)
 
