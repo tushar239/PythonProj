91a16898075fed359e6e4d7f1293675f56d513f8
Tushar Chokshi <tushar239@yahoo.com>
2024-03-08 06:21:27 -0800

    commit

---
+++ inheritance/MultipleInheritance3.py
@@ -0,0 +1,38 @@
+# https://www.geeksforgeeks.org/multiple-inheritance-in-python/
+
+# when we try to call the method m for Class1,
+# Class2, Class3 from the method m of Class4
+
+class Class1:
+    def m(self):
+        print("In Class1")
+
+
+class Class2(Class1):
+    def m(self):
+        print("In Class2")
+
+
+class Class3(Class1):
+    def m(self):
+        print("In Class3")
+
+
+class Class4(Class2, Class3):
+    def m(self):
+        print("In Class4")
+        Class2.m(self)
+        Class3.m(self)
+        Class1.m(self)
+
+
+obj = Class4()
+obj.m()
+
+'''
+O/P:
+In Class4
+In Class2
+In Class3
+In Class1
+'''
