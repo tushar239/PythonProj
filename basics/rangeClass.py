e8de4ad5cdc503ef7af8c2ad89b61cc3a28cf450
Tushar Chokshi <tushar239@yahoo.com>
2024-03-17 09:25:43 -0700

    commit

--- basics/rangeClass.py
+++ basics/rangeClass.py
@@ -1,13 +1,13 @@
-num_range = range(0, 10, 1)
-print(type(num_range)) # <class 'range'>
-print(num_range) # range(0, 10)
-
 '''
-'range' is a class, not a function. 
+'range' is a class, not a function in builtins.py
+
+It returns an object that produces a sequence of integers from start (inclusive)
+to stop (exclusive) by step.  range(i, j) produces i, i+1, i+2, ..., j-1.
 
 The advantage of the range type over a regular list or tuple is that a range object will always take the same (small) amount of memory, 
 no matter the size of the range it represents (as it only stores the start, stop and step values, calculating individual items and subranges as needed).
 '''
+
 num_range = range(0, 15, 2)  # start=0, stop=5, step=1
 print(type(num_range)) # <class 'range'>
 print(num_range)  # range(0, 15, 2)
