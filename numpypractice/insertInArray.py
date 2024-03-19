e069ce55998936bbf2b860eb6d6236da12351fc3
Tushar Chokshi <tushar239@yahoo.com>
2024-03-19 02:31:36 -0700

    commit

--- numpypractice/insertInArray.py
+++ numpypractice/insertInArray.py
@@ -1,9 +1,9 @@
 import numpy as np
-
 '''
-append(array, values, axis) - it will add the new elements at the end of given array and will produce a new array
+insert(array, obj, values, axis) adds values at a given position and axis in an array.
 
 array - input array
+obj - index position
 values = array of values to be inserted
 axis - axis along which values should be inserted
 
@@ -11,23 +11,23 @@ axis = 0 means x-axis(horizontal axis), 1 means y-axis(vertical axis)
 if axis=none, then it will flatten 2D array to 1D array alon with new elements that you added.
 '''
 
-# Adding a new row
 a = np.array([[1,2,3], [4,5,6], [7,8,9]])
-b = np.append(a, [[10,11,14]], 0) # axis=0 for adding as a row
+b = np.insert(a, 1, [10,11,12], 0)
 
 print(b)
 '''
 [[ 1  2  3]
+ [10 11 12]
  [ 4  5  6]
- [ 7  8  9]
- [10 11 14]]
+ [ 7  8  9]]
 '''
-c = np.array([10,11,14]).reshape((3, 1))
-d = np.append(a, c, 1) # axis=1 for adding as a col
-print(d)
 
+c = np.array([10,11,12])
+print(c)
+d = np.insert(a, 1, c, 1)
+print(d)
 '''
-[[ 1  2  3 10]
- [ 4  5  6 11]
- [ 7  8  9 14]]
+[[ 1 10  2  3]
+ [ 4 11  5  6]
+ [ 7 12  8  9]]
 '''
