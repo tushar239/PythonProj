7c7aa3df6ba7eb5ae8e6a4cc0bbbfcbe68a48931
Tushar Chokshi <tushar239@yahoo.com>
2024-03-26 05:50:23 -0700

    commit

--- numpymatrix/createMatrix.py
+++ numpymatrix/createMatrix.py
@@ -8,6 +8,24 @@ print(a)
  [ 7  8  9 10]]
 '''
 
+# Second way to create a matrix
+b = np.matrix([[1, 2, 3, 4], [4, 5, 6, 7], [7, 8, 9, 10]])
+print(b)
+'''
+[[ 1  2  3  4]
+ [ 4  5  6  7]
+ [ 7  8  9 10]]
+'''
+
+# Third way to create a matrix - Instead of using matrix class, use array method
+c = np.array([[1, 2, 3, 4], [4, 5, 6, 7], [7, 8, 9, 10]])
+print(c)
+'''
+[[ 1  2  3  4]
+ [ 4  5  6  7]
+ [ 7  8  9 10]]
+'''
+
 print(a.shape)  # (3, 4)
 # array.shape returns a tuple. So, you can access a tuple to know number of rows and cols
 print(a.shape[0])  # gives number of rows - 3
