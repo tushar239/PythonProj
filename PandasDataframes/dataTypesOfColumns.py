770169e4e8628a87ecf09ba0f9a8fccc840de21a
Tushar Chokshi <tushar239@yahoo.com>
2024-04-10 21:06:48 -0700

    commit

--- PandasDataframes/dataTypesOfColumns.py
+++ PandasDataframes/dataTypesOfColumns.py
@@ -34,3 +34,71 @@ dtype: object
 # Gives count of unique data types
 # countOfUniqueDataTypes = cars_data.get_dtype_counts() # this method is not available
 # print(countOfUniqueDataTypes)
+
+'''
+select columns based on their data types.
+'''
+cols = cars_data.select_dtypes(include=[int])
+print(cols)
+'''
+     Price  Automatic    CC  Weight
+0     13500          0  2000    1165
+1     13750          0  2000    1165
+2     13950          0  2000    1165
+3     14950          0  2000    1165
+4     13750          0  2000    1170
+...     ...        ...   ...     ...
+1431   7500          0  1300    1025
+1432  10845          0  1300    1015
+1433   8500          0  1300    1015
+1434   7250          0  1300    1015
+1435   6950          0  1600    1114
+
+[1436 rows x 4 columns]
+'''
+
+
+cols = cars_data.select_dtypes(exclude=[object, int])
+print(cols)
+'''
+      Age       KM  MetColor
+0     23.0  46986.0       1.0
+1     23.0  72937.0       1.0
+2     24.0  41711.0       NaN
+3     26.0  48000.0       0.0
+4     30.0  38500.0       0.0
+...    ...      ...       ...
+1431   NaN  20544.0       1.0
+1432  72.0      NaN       0.0
+1433   NaN  17016.0       0.0
+1434  70.0      NaN       1.0
+1435  76.0      1.0       0.0
+
+[1436 rows x 3 columns]
+'''
+
+'''
+Get concise summary of dataframe
+'''
+summary =  cars_data.info()
+print(summary)
+'''
+<class 'pandas.core.frame.DataFrame'>
+Index: 1436 entries, 0 to 1435
+Data columns (total 10 columns):
+ #   Column     Non-Null Count  Dtype  
+---  ------     --------------  -----  
+ 0   Price      1436 non-null   int64  
+ 1   Age        1336 non-null   float64
+ 2   KM         1421 non-null   float64
+ 3   FuelType   1336 non-null   object 
+ 4   HP         1436 non-null   object 
+ 5   MetColor   1286 non-null   float64
+ 6   Automatic  1436 non-null   int64  
+ 7   CC         1436 non-null   int64  
+ 8   Doors      1436 non-null   object 
+ 9   Weight     1436 non-null   int64  
+dtypes: float64(3), int64(4), object(3)
+memory usage: 123.4+ KB
+None
+'''
