5f5f773444f01462253834154f855323ef1d6900
Tushar Chokshi <tushar239@yahoo.com>
2024-04-16 05:23:49 -0700

    commit

--- basics/equalityComparision.py
+++ basics/equalityComparision.py
@@ -1,1 +1,5 @@
 # https://www.geeksforgeeks.org/difference-between-__eq__-vs-is-vs-in-python/
+'''
+The “==” operator compares the value or equality of two objects,
+whereas the Python “is” operator checks whether two variables point to the same object in memory.
+'''
