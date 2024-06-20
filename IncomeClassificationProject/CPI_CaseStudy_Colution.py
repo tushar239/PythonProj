6f47cdf7c9c35e634b2ace0ffd4fec4f4723f66b
Tushar Chokshi <tushar239@yahoo.com>
2024-06-20 06:21:34 -0700

    commit

--- IncomeClassificationProject/CPI_CaseStudy_Colution.py
+++ IncomeClassificationProject/CPI_CaseStudy_Colution.py
@@ -209,7 +209,7 @@ JobType and occupation have missing values
 # data.isnull() returns a dataframe with True and False values in each cell
 # True means null value and False means not-null value
 # any() returns a series of index numbers
-rows_with_missing_values = data[data.isnull().any(axis=1)] # axis=1 is to consider at least one column with NaN value
+rows_with_missing_values = data[data.isnull().any(axis=1)] # axis=1 is to consider at least one COLUMN(not row) with NaN value
 #print("Rows with missing values:\n", rows_with_missing_values.to_string())
 print("Rows with missing values:\n", rows_with_missing_values)
 """
@@ -286,7 +286,12 @@ rows_with_missing_jobtype_and_occupation = jobtype_and_occupation_data\
     .where(jobtype_and_occupation_data['JobType'].isnull() &
            jobtype_and_occupation_data['occupation'].isnull())
 '''
+'''
 # works
+exp1 & exp2                # Element-wise logical AND
+exp1 | exp2                # Element-wise logical OR
+~exp1                      # Element-wise logical NOT
+'''
 rows_with_missing_jobtype_and_occupation = jobtype_and_occupation_data[
                                                 jobtype_and_occupation_data['JobType'].isnull() &
                                                 jobtype_and_occupation_data['occupation'].isnull()
@@ -330,4 +335,21 @@ rows_with_missing_occupation_and_with_jobtype.to_excel('rows_with_missing_occupa
 2. Missing values in Occupation = 1816
 3. There are 1809 rows where two specific columns (i.e. occupation & JobType) have missing values
 4. (1816-1809) = 7 => You still have occupation unfilled for these 7 rows because JobType is 'Never-worked'
+"""
+# We will drop all the rows where there are missing values because there are only 1816 rows like that out of 32k rows
+# axis=0 means drop all those ROWs that have missing values
+# axis=1 means drop all those COLUMNs that have missing values
+data2 = data.dropna(axis=0)
+total_rows_left = data2.shape[0]
+print("After dropping the rows with missing values, total rows left: ", total_rows_left) # 30162
+
+""" Just testing
+something = ~jobtype_and_occupation_data['JobType'].isnull() & jobtype_and_occupation_data['occupation'].isnull()
+print(type(something)) # <class 'pandas.core.series.Series'>
+print(something.__repr__())
+something_else = data[(0, False) (1, False) (2, False) (3, False) (4, False) (5, False) (6, False) (7, False) (8, False) (9, False) (10, False) (11, False) (12, False) (13, False) (14, False) (15, False) (16, False) (17, False) (18, False) (19, False) (20, False) (21, False) (22, False) (23, False) (24, False) (25, False) (26, False) (27, False) (28, False) (29, False) (30, False) (31, False) (32, False) (33, False) (34, False) (35, False) (36, False) (37, False) (38, False) (39, False) (40, False) (41, False) (42, False) (43, False) (44, False) (45, False) (46, False) (47, False) (48, False) (49, False) (50, False) (51, False) (52, False) (53, False) (54, False) (55, False) (56, False) (57, False) (58, False) (59, False) (60, False) (61, False) (62, False) (63, False) (64, False) (65, False) (66, False) (67, False) (68, False) (69, False) (70, False) (71, False) (72, False) (73, False) (74, False) (75, False) (76, False) (77, False) (78, False) (79, False) (80, False) (81, False) (82, False) (83, False)]
+print(type(something_else))
+
+something = jobtype_and_occupation_data[pd.Series({0: False, 1:True})]
+print(something)
 """
