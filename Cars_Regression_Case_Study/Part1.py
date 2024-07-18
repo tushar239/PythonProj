2e49ca6e23fa041b9a3c9a57426f5dc6623a5b49
Tushar Chokshi <tushar239@yahoo.com>
2024-07-17 20:10:24 -0700

    commit

---
+++ Cars_Regression_Case_Study/Part1.py
@@ -0,0 +1,11 @@
+# To work with dataframes
+import pandas as pd
+
+# To perform numerical operations
+import numpy as np
+
+# To visualize data
+import seaborn as sns
+import matplotlib.pyplot as plt  # pyplot means python plot
+
+import gotoDataDir
