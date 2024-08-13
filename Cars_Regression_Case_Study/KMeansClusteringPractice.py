303846c5fe627bc612134ee433f4097577507ca4
Tushar Chokshi <tushar239@yahoo.com>
2024-08-13 02:28:33 -0700

    commit

---
+++ Cars_Regression_Case_Study/KMeansClusteringPractice.py
@@ -0,0 +1,3 @@
+# K-Means Clustering - https://www.geeksforgeeks.org/zip-in-python/
+# Elbow method to decide right number of clusters - https://www.geeksforgeeks.org/elbow-method-for-optimal-value-of-k-in-kmeans/
+# zip() method - https://www.geeksforgeeks.org/zip-in-python/
---
+++ Cars_Regression_Case_Study/ReinforcementQLearningPractice.py
@@ -0,0 +1,3 @@
+# Q-Learning - https://www.geeksforgeeks.org/q-learning-in-python/?ref=previous_article
+# Python Implementation using Q-learning - https://www.geeksforgeeks.org/ml-reinforcement-learning-algorithm-python-implementation-using-q-learning/
+# Deep Q-learning - https://www.geeksforgeeks.org/deep-q-learning/?ref=next_article
