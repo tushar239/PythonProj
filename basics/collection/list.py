b2ca40b8beded6b4d81b446060284c68712f065e
Tushar Chokshi <tushar239@yahoo.com>
2024-09-04 04:14:06 -0700

    commit

--- basics/collection/list.py
+++ basics/collection/list.py
@@ -125,3 +125,66 @@ del thislist
 thislist = ["apple", "banana", "cherry"]
 thislist.clear()
 print(thislist) # []
+
+############## Looping the list ###############
+# Looping by item (like a smart for loop in java)
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+
+# Looping by index number (like an old for loop in java)
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+################ List comprehension ################
+
+'''
+syntax
+newlist = [expression for item in list if condition == True]
+'''
+
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
+
+newlist = [x for x in thislist]
+print(newlist) # ['apple', 'banana', 'cherry']
+
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+print(newlist)
+# is same as
+newlist = [x for x in fruits if "a" in x]
+print(newlist)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+for x in fruits:
+    if x != 'banana':
+        newlist.append(x)
+    else:
+        newlist.append('orange')
+print(newlist) # ['apple', 'orange', 'cherry', 'kiwi', 'mango']
+# is same as
+newlist = [x if x != "banana" else "orange" for x in fruits]
+print(newlist) # ['apple', 'orange', 'cherry', 'kiwi', 'mango']
+
+newlist = [x for x in fruits if x != "apple"]
+
+newlist = [x for x in range(10)]
+
+newlist = [x for x in range(10) if x < 5]
+
+newlist = [x.upper() for x in fruits]
+
+newlist = ['hello' for x in fruits]
