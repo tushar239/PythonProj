70ebedf1a4b4733df7670f1948b00dbbab396ff3
Tushar Chokshi <tushar239@yahoo.com>
2024-09-24 05:33:38 -0700

    commit

--- basics/geospatialdata/_0plotting_polygon.py
+++ basics/geospatialdata/_0plotting_polygon.py
@@ -2,6 +2,8 @@ from shapely.geometry import Polygon
 import matplotlib.pyplot as plt
 import geopandas as gpd
 
+# https://stackoverflow.com/questions/55522395/how-do-i-plot-shapely-polygons-and-objects-using-matplotlib
+
 polygon1 = Polygon([(0,5),
                     (1,1),
                     (3,0),
