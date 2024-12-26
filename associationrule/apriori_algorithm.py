cd14c40ee991ac7154187b022eee84f73f3fe306
Tushar Chokshi <tushar239@yahoo.com>
2024-12-26 04:59:13 -0800

    initial commit

---
+++ associationrule/apriori_algorithm.py
@@ -0,0 +1,5 @@
+'''
+https://www.youtube.com/watch?v=q4J1YupRFC0&list=PLm5IZukmvRNmj2eqq9Y0_7GRP_4dozIGZ&index=5
+https://www.youtube.com/watch?v=LTLNiL6EWt8&list=PLm5IZukmvRNmj2eqq9Y0_7GRP_4dozIGZ&index=6
+https://www.youtube.com/watch?v=bury6r8_rYs&list=PLm5IZukmvRNmj2eqq9Y0_7GRP_4dozIGZ&index=7
+'''
