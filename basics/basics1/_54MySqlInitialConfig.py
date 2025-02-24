747ca16c8c369f378b562020d683d30bf699ffbb
Tushar Chokshi <tushar239@yahoo.com>
2025-02-24 00:17:10 -0800

    first commit

--- basics/basics1/_54MySqlInitialConfig.py
+++ basics/basics1/_54MySqlInitialConfig.py
@@ -1,6 +1,8 @@
 import mysql.connector
 
 """
+install mysql-connector-python module
+
 The connection has one purpose: controlling access to the database. 
 The cursor has one purpose: keeping track of where we are in the database so that if several programs are accessing the database at the same time, the database can keep track of who is trying to do what.
 
@@ -10,7 +12,6 @@ This temporary work area is used to store the data retrieved from the database, 
 This cursor is on DB side and records are fetched one by one lazily, unless you say connection.cursor(buffered=true). 
 When you buffer it, all the records will be fetched from DB cursor and kept in memory.
 """
-
 connection = mysql.connector.connect(
     host="localhost",
     user="admin",
