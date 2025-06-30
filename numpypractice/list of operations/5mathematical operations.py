caeb874f674cb857772a4e9d72a035249e5a4052
Tushar Chokshi <tushar239@yahoo.com>
2025-06-30 05:41:04 -0700

    first commit

--- numpypractice/list of operations/5mathematical operations.py
+++ numpypractice/list of operations/5mathematical operations.py
@@ -1,5 +1,24 @@
+'''
 a + b
 a - b
 a * b        # element-wise multiplication
 a / b
 a ** 2       # power
+'''
+
+import numpy as np
+
+a = np.array([[1, 2],
+              [3, 4]])
+b = np.array([[11, 22],
+              [33, 44]])
+print(a+b)
+'''
+[[12 24]
+ [36 48]]
+'''
+print(a*b)
+'''
+[[ 11  44]
+ [ 99 176]]
+'''
