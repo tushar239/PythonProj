cbaea594d6cf4b48320f87a8ff5239eacf21afb2
Tushar Chokshi <tushar239@yahoo.com>
2025-06-30 05:41:08 -0700

    first commit

--- numpypractice/list of operations/6matrix multiplication.py
+++ numpypractice/list of operations/6matrix multiplication.py
@@ -1,2 +1,13 @@
-np.dot(a, b)
-a @ b
+import numpy as np
+
+a = np.array([[1, 2],
+              [3, 4]])
+b = np.array([[11, 22],
+              [33, 44]])
+
+print(np.dot(a, b))
+print(a @ b)
+'''
+[[ 77 110]
+ [165 242]]
+'''
