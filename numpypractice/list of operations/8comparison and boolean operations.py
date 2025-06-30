292eec423751120fc48fe7a165c2e2dd3936356b
Tushar Chokshi <tushar239@yahoo.com>
2025-06-30 05:44:08 -0700

    first commit

--- numpypractice/list of operations/8comparison and boolean operations.py
+++ numpypractice/list of operations/8comparison and boolean operations.py
@@ -1,7 +1,9 @@
+import numpy as np
+
 a = np.array([1, 2, 3])
 b = np.array([2, 2, 2])
 
-a > b        # [False, False, True]
-np.where(a > b, 1, 0)   # condition-based selection
-np.any(a > 2)           # True
-np.all(a > 0)           # True
+print(a > b)       # [False, False, True]
+print(np.where(a > b, 1, 0))   # condition-based selection [0 0 1]
+print(np.any(a > 2))          # True
+print(np.all(a > 0))           # True
