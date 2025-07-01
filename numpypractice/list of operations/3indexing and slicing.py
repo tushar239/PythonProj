d223ca3fd151bb553edbcbf43cc9752132f937ae
Tushar Chokshi <tushar239@yahoo.com>
2025-07-01 01:08:49 -0700

    first commit

--- numpypractice/list of operations/3indexing and slicing.py
+++ numpypractice/list of operations/3indexing and slicing.py
@@ -1,8 +1,26 @@
 import numpy as np
-
+'''
 a = np.array([[1, 2, 3], [4, 5, 6]])
 
 a[0, 1]       # element at row 0, column 1
 a[:, 1]       # all rows, column 1
 a[1, :]       # row 1, all columns
 a[0:2, 1:]    # slicing subarray
+'''
+X = np.array([[1, 2], [2, 2], [2, 3], [8, 7], [8, 8], [25, 80]])
+a = X[:, 0] # 0th column values from all rows
+print(a) # [ 1  2  2  8  8 25]
+b = X[:, 1]  # 1st column values from all rows
+print(b) # [ 2  2  3  7  8 80]
+c = X[:, 0:2]
+print(c)
+'''
+[[ 1  2]
+ [ 2  2]
+ [ 2  3]
+ [ 8  7]
+ [ 8  8]
+ [25 80]]
+'''
+d = X[2:4, 0] # 0th column data from 2nd and 3rd row
+print(d) # [2, 8]
--- outliers_finding_strategies/dbscan_method.py
+++ outliers_finding_strategies/dbscan_method.py
@@ -1,6 +1,24 @@
+'''
+DBSCAN (Density-Based Spatial Clustering of Applications with Noise)
+
+DBSCAN groups together points that are close to each other (i.e., dense regions) and marks points in sparse regions as outliers.
+
+eps	- Radius to search for neighboring points
+min_samples	- Minimum number of points required to form a dense region (cluster)
+Core Point - A point with at least min_samples neighbors within eps
+Border Point - Fewer neighbors than min_samples, but in the neighborhood of a core
+Noise Point	- Not a core or border — outlier
+
+Tips for Use
+Choosing eps is critical! Use k-distance graph to tune it.
+Too small eps: Too many outliers.
+Too large eps: All points may be one big cluster.
+'''
+
 from sklearn.cluster import DBSCAN
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Sample DataFrame
 df = pd.DataFrame({
@@ -17,3 +35,19 @@ print(df[df['outlier'] == -1])
    age  outlier
 6  100       -1
 '''
+##########################################################33
+# Sample dataset with one outlier
+X = np.array([[1, 2], [2, 2], [2, 3], [8, 7], [8, 8], [25, 80]])  # last point is likely outlier
+
+db = DBSCAN(eps=3, min_samples=2).fit(X)
+labels = db.labels_  # -1 indicates outliers
+
+# Plotting
+plt.scatter(X[:, 0], X[:, 1], c=labels, cmap='plasma', s=100)
+plt.title("DBSCAN Clustering with Outlier Detection")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.show()
+
+# View which points are outliers
+print("Outlier indices:", np.where(labels == -1)[0])
