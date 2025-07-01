7a70c27b6afafe01790b1f60f9c8c3b5bcf65755
Tushar Chokshi <tushar239@yahoo.com>
2025-07-01 01:49:08 -0700

    first commit

--- outliers_finding_strategies/isolation_forest.py
+++ outliers_finding_strategies/isolation_forest.py
@@ -48,3 +48,43 @@ df.drop(indices_to_drop, inplace=True)
 sns.boxplot(y=df['age'])
 plt.title("Box Plot of age after dropping outlier rows")
 plt.show()
+
+################################################################
+# finding outlier observation with multi-variables data
+df = pd.DataFrame({
+    'age': [15, 16, 14, 17, 18, 16, 100],  # (100,2000) is an outlier
+    'year': [1978, 1979, 1974, 1975, 1976, 1977, 2000]
+})
+
+iso = IsolationForest(contamination=0.1) # contamination=0.1 means we expect ~10% of points to be outliers.
+df['outlier'] = iso.fit_predict(df)
+
+# -1 = outlier, 1 = inlier
+print(df[df['outlier'] == -1])
+'''
+   age  year  outlier
+6  100  2000       -1
+'''
+
+# First subplot (left)
+plt.subplot(1, 2, 1)
+sns.boxplot(y=df['age'])
+plt.title("Age")
+
+# Second subplot (right)
+plt.subplot(1, 2, 2)
+sns.boxplot(y=df['year'])
+plt.title("Year")
+
+plt.tight_layout()  # Adjust spacing
+plt.show()
+
+iso = IsolationForest(contamination=0.1) # contamination=0.1 means we expect ~10% of points to be outliers.
+df['outlier2'] = iso.fit_predict(df)
+
+# -1 = outlier, 1 = inlier
+print(df[df['outlier2'] == -1])
+'''
+   age  year  outlier  outlier2
+6  100  2000       -1        -1
+'''
