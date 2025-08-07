7757882b2b9825cb21ee48b6124b1af507922a9a
Tushar Chokshi <tushar239@yahoo.com>
2025-08-07 01:09:18 -0700

    first commit

---
+++ Neural_Networks/ANN/ann_regression_project.py
@@ -0,0 +1,83 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+print(tf.__version__)
+
+dataset = pd.read_excel('Folds5x2_pp.xlsx')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+print(y)
+'''
+[463.26 444.37 488.56 ... 429.57 435.74 453.28]
+'''
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+ann = tf.keras.models.Sequential()
+
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+
+'''
+For the output layer of a regression model, a linear activation function is generally appropriate. 
+This function simply outputs the input value directly, without any transformation or squashing. 
+This allows the model to predict a continuous range of numerical values. 
+Using a linear activation function is crucial in regression tasks because it preserves the real-valued nature of the predictions. 
+
+The default activation function for a tf.keras.layers.Dense layer in Keras is the linear activation function.
+'''
+ann.add(tf.keras.layers.Dense(units=1, activation='linear'))
+
+ann.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+ann.fit(X_train, y_train, batch_size = 32, epochs = 100)
+
+'''
+Epoch 1/100
+240/240 [==============================] - 0s 2ms/step - loss: 82110.9922
+Epoch 2/100
+240/240 [==============================] - 0s 1ms/step - loss: 656.9040
+Epoch 3/100
+240/240 [==============================] - 0s 1ms/step - loss: 429.4050
+Epoch 4/100
+240/240 [==============================] - 0s 1ms/step - loss: 417.9655
+Epoch 5/100
+240/240 [==============================] - 0s 1ms/step - loss: 405.0805
+Epoch 6/100
+240/240 [==============================] - 0s 1ms/step - loss: 388.9652
+Epoch 7/100
+240/240 [==============================] - 0s 2ms/step - loss: 371.9583
+Epoch 8/100
+240/240 [==============================] - 0s 1ms/step - loss: 353.7564
+Epoch 9/100
+240/240 [==============================] - 0s 1ms/step - loss: 332.1931
+Epoch 10/100
+240/240 [==============================] - 0s 1ms/step - loss: 310.8850
+Epoch 11/100
+240/240 [==============================] - 0s 1ms/step - loss: 288.6723
+Epoch 12/100
+240/240 [==============================] - 0s 1ms/step - loss: 265.4893
+Epoch 13/100
+...
+Epoch 99/100
+240/240 [==============================] - 0s 1ms/step - loss: 26.8439
+Epoch 100/100
+240/240 [==============================] - 0s 1ms/step - loss: 26.7392
+'''
+
+y_pred = ann.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+'''
+[[429.1  431.23]
+ [460.12 460.01]
+ [463.61 461.14]
+ ...
+ [470.85 473.26]
+ [437.71 438.  ]
+ [456.9  463.28]]
+'''
