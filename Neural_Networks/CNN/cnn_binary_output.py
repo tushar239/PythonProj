70d63df0421d92f1725d8334f99462545b1756c3
Tushar Chokshi <tushar239@yahoo.com>
2025-08-31 06:39:50 -0700

    first commit

--- Neural_Networks/CNN/cnn_binary_output.py
+++ Neural_Networks/CNN/cnn_binary_output.py
@@ -98,11 +98,16 @@ cnn.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
 
 # ############## Part 3 - Training the CNN #################
 # Compiling the CNN
-#'adam' optimizer performs Stochastic Gradient Descent.
-#https://www.geeksforgeeks.org/deep-learning/optimizers-in-tensorflow/
-# we have binary output, so loss function is binary_corssentropy
-# Read 'crossentropy loss function' section in 'Udemy course.docx'
-# accuracy metrics is the most relevant way to measure the performance of classification problem
+'''
+Optimizer:
+Read 'Optimizers (Gradient Descents)' section in 'Udemy Course.docx'
+
+Loss Function:
+Read 'crossentropy loss function' section in 'Udemy Course.docx'
+
+Performance metrics:
+Read 'Performance metrics' section in 'Udemy Course.docx'
+'''
 cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 # Training the CNN on the Training set and evaluating it on the Test set
@@ -110,6 +115,61 @@ cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accura
 # This is a bit different from what we used to before
 # We used to train the NN with epochs and then used to evaluate just once.
 cnn.fit(x = training_set, validation_data = test_set, epochs = 25)
+'''
+Note that with each epoch, accuracy is increasing and loss is decreasing.
+
+Epoch 1/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 163s 643ms/step - accuracy: 0.5631 - loss: 0.6795 - val_accuracy: 0.5890 - val_loss: 0.6659
+Epoch 2/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 49s 197ms/step - accuracy: 0.6680 - loss: 0.6095 - val_accuracy: 0.6370 - val_loss: 0.6256
+Epoch 3/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 47s 188ms/step - accuracy: 0.7036 - loss: 0.5639 - val_accuracy: 0.6905 - val_loss: 0.5808
+Epoch 4/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 48s 192ms/step - accuracy: 0.7294 - loss: 0.5289 - val_accuracy: 0.7625 - val_loss: 0.5126
+Epoch 5/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 40s 161ms/step - accuracy: 0.7516 - loss: 0.5039 - val_accuracy: 0.7685 - val_loss: 0.4945
+Epoch 6/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 41s 166ms/step - accuracy: 0.7585 - loss: 0.4854 - val_accuracy: 0.7710 - val_loss: 0.4993
+Epoch 7/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 42s 169ms/step - accuracy: 0.7772 - loss: 0.4629 - val_accuracy: 0.7730 - val_loss: 0.4879
+Epoch 8/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 26s 104ms/step - accuracy: 0.7946 - loss: 0.4390 - val_accuracy: 0.7815 - val_loss: 0.4690
+Epoch 9/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 23s 92ms/step - accuracy: 0.8004 - loss: 0.4327 - val_accuracy: 0.7870 - val_loss: 0.4697
+Epoch 10/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 27s 109ms/step - accuracy: 0.8076 - loss: 0.4113 - val_accuracy: 0.7610 - val_loss: 0.5323
+Epoch 11/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 30s 119ms/step - accuracy: 0.8080 - loss: 0.4077 - val_accuracy: 0.7820 - val_loss: 0.4610
+Epoch 12/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 26s 104ms/step - accuracy: 0.8271 - loss: 0.3838 - val_accuracy: 0.7815 - val_loss: 0.4815
+Epoch 13/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 23s 92ms/step - accuracy: 0.8267 - loss: 0.3804 - val_accuracy: 0.7840 - val_loss: 0.5251
+Epoch 14/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 23s 91ms/step - accuracy: 0.8396 - loss: 0.3590 - val_accuracy: 0.8105 - val_loss: 0.4633
+Epoch 15/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 27s 109ms/step - accuracy: 0.8526 - loss: 0.3373 - val_accuracy: 0.7810 - val_loss: 0.5106
+Epoch 16/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 37s 147ms/step - accuracy: 0.8597 - loss: 0.3289 - val_accuracy: 0.7880 - val_loss: 0.5466
+Epoch 17/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 37s 149ms/step - accuracy: 0.8689 - loss: 0.3088 - val_accuracy: 0.8060 - val_loss: 0.5005
+Epoch 18/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 30s 118ms/step - accuracy: 0.8708 - loss: 0.2942 - val_accuracy: 0.7990 - val_loss: 0.4802
+Epoch 19/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 32s 128ms/step - accuracy: 0.8878 - loss: 0.2725 - val_accuracy: 0.7860 - val_loss: 0.5836
+Epoch 20/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 39s 157ms/step - accuracy: 0.8898 - loss: 0.2616 - val_accuracy: 0.7985 - val_loss: 0.5143
+Epoch 21/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 38s 151ms/step - accuracy: 0.8961 - loss: 0.2524 - val_accuracy: 0.8010 - val_loss: 0.5212
+Epoch 22/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 39s 158ms/step - accuracy: 0.9009 - loss: 0.2365 - val_accuracy: 0.8055 - val_loss: 0.5206
+Epoch 23/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 29s 115ms/step - accuracy: 0.9103 - loss: 0.2241 - val_accuracy: 0.8080 - val_loss: 0.5212
+Epoch 24/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 31s 125ms/step - accuracy: 0.9140 - loss: 0.2131 - val_accuracy: 0.8060 - val_loss: 0.5402
+Epoch 25/25
+250/250 ━━━━━━━━━━━━━━━━━━━━ 42s 166ms/step - accuracy: 0.9209 - loss: 0.1995 - val_accuracy: 0.8000 - val_loss: 0.5762
+1/1 ━━━━━━━━━━━━━━━━━━━━ 0s 318ms/step
+'''
 '''
 # Trying to fit training_set, evaluating test_set and predicting some images(test_set) just like how we did in ann. 
 cnn.fit(x = training_set, epochs = 1)
@@ -127,10 +187,12 @@ print(y_pred)
 import numpy as np
 from tensorflow.keras.preprocessing import image
 
+# target_size should be same as what we used while creating training_set
 test_image = image.load_img('dataset/single_prediction/cat_or_dog_3.jpg', target_size = (64, 64))
-test_image = image.img_to_array(test_image)
+# Read 'image.img_to_array()' section in 'Udemy Course.docx'
+test_image = image.img_to_array(test_image) # converts pil format image to numpy array
 test_image = np.expand_dims(test_image, axis = 0)
-result = cnn.predict(test_image)
+result = cnn.predict(test_image) # predict method expects input as 2-D array
 print(result) # [[1.]]
 #training_set.class_indices
 
