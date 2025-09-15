1f8123527c295d140cdfcbe9d1f6f97693512bb3
Tushar Chokshi <tushar239@yahoo.com>
2025-09-14 20:48:55 -0700

    first commit

--- Neural_Networks/ANN/reshape_test.py
+++ Neural_Networks/ANN/reshape_test.py
@@ -26,3 +26,54 @@ print(b_1D_array)
  [False]
  [False]]
 '''
+
+'''
+In Python's NumPy library, reshape(-1, 1) is used to transform an array into a column vector.
+Here is a breakdown of what reshape(-1, 1) means:
+-1: This placeholder tells NumPy to automatically calculate the size of that dimension based on the total number of elements in the array and the other specified dimensions. In this case, it will determine the number of rows needed to accommodate all elements.
+1: This explicitly sets the second dimension (columns) to a size of 1.
+
+When you apply array.reshape(-1, 1) to a 1D array or a multi-dimensional array, the result will be a 2D array with a single column, and the number of rows will be determined by the total number of elements in the original array.
+'''
+arr_1d = np.array([1, 2, 3, 4, 5, 6])
+print("Original 1D array:")
+print(arr_1d)
+print("Shape:", arr_1d.shape)
+
+# Reshape to a column vector
+column_vector = arr_1d.reshape(-1, 1)
+print("\nReshaped column vector:")
+print(column_vector)
+print("Shape:", column_vector.shape)
+'''
+Reshaped column vector:
+[[1]
+ [2]
+ [3]
+ [4]
+ [5]
+ [6]]
+Shape: (6, 1)
+'''
+
+# A 2D array
+arr_2d = np.array([[1, 2, 3], [4, 5, 6]])
+print("\nOriginal 2D array:")
+print(arr_2d)
+print("Shape:", arr_2d.shape)
+
+# Reshape to a column vector
+column_vector_from_2d = arr_2d.reshape(-1, 1)
+print("\nReshaped column vector from 2D array:")
+print(column_vector_from_2d)
+print("Shape:", column_vector_from_2d.shape)
+'''
+Reshaped column vector from 2D array:
+[[1]
+ [2]
+ [3]
+ [4]
+ [5]
+ [6]]
+Shape: (6, 1)
+'''
---
+++ Neural_Networks/RNN/rnn.py
@@ -0,0 +1,284 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the training set
+dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
+training_set = dataset_train.iloc[:, 1:2].values
+
+# Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+training_set_scaled = sc.fit_transform(training_set)
+
+# Creating a data structure with 60 timesteps and 1 output
+X_train = []
+y_train = []
+for i in range(60, 1258):
+    X_train.append(training_set_scaled[i-60:i, 0])
+    y_train.append(training_set_scaled[i, 0])
+X_train, y_train = np.array(X_train), np.array(y_train)
+
+# Reshaping
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
+# **************** Part 2 - Building and Training the RNN ************
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+regressor = Sequential()
+regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+regressor.add(Dropout(0.2))
+
+# Adding a second LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+# Adding a third LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+# Adding a fourth LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 50))
+regressor.add(Dropout(0.2))
+
+# Adding the output layer
+regressor.add(Dense(units = 1))
+
+# Compiling the RNN
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+# Fitting the RNN to the Training set
+regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
+
+'''
+Epoch 1/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 11s 105ms/step - loss: 0.0881
+Epoch 2/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 130ms/step - loss: 0.0063
+Epoch 3/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 105ms/step - loss: 0.0058
+Epoch 4/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 103ms/step - loss: 0.0048
+Epoch 5/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 6s 137ms/step - loss: 0.0054
+Epoch 6/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 9s 102ms/step - loss: 0.0046
+Epoch 7/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 6s 127ms/step - loss: 0.0038
+Epoch 8/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 102ms/step - loss: 0.0043
+Epoch 9/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 107ms/step - loss: 0.0042
+Epoch 10/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 6s 118ms/step - loss: 0.0036
+Epoch 11/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 101ms/step - loss: 0.0039
+Epoch 12/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 112ms/step - loss: 0.0045
+Epoch 13/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 113ms/step - loss: 0.0035
+Epoch 14/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 101ms/step - loss: 0.0040
+Epoch 15/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 129ms/step - loss: 0.0042
+Epoch 16/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 104ms/step - loss: 0.0033
+Epoch 17/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 101ms/step - loss: 0.0033
+Epoch 18/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 126ms/step - loss: 0.0041
+Epoch 19/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 104ms/step - loss: 0.0033
+Epoch 20/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 102ms/step - loss: 0.0032
+Epoch 21/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 6s 138ms/step - loss: 0.0039
+Epoch 22/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 9s 110ms/step - loss: 0.0030
+Epoch 23/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 133ms/step - loss: 0.0032
+Epoch 24/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 104ms/step - loss: 0.0042
+Epoch 25/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 6s 116ms/step - loss: 0.0035
+Epoch 26/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 122ms/step - loss: 0.0032
+Epoch 27/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 103ms/step - loss: 0.0029
+Epoch 28/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 6s 133ms/step - loss: 0.0028
+Epoch 29/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 101ms/step - loss: 0.0041
+Epoch 30/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 102ms/step - loss: 0.0030
+Epoch 31/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 6s 138ms/step - loss: 0.0031
+Epoch 32/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 9s 110ms/step - loss: 0.0028
+Epoch 33/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 113ms/step - loss: 0.0033
+Epoch 34/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 102ms/step - loss: 0.0031
+Epoch 35/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 7s 144ms/step - loss: 0.0030
+Epoch 36/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 9s 101ms/step - loss: 0.0027
+Epoch 37/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 6s 133ms/step - loss: 0.0029
+Epoch 38/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 102ms/step - loss: 0.0025
+Epoch 39/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 101ms/step - loss: 0.0022
+Epoch 40/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 137ms/step - loss: 0.0024
+Epoch 41/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 9s 107ms/step - loss: 0.0027
+Epoch 42/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 6s 117ms/step - loss: 0.0022
+Epoch 43/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 103ms/step - loss: 0.0025
+Epoch 44/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 6s 134ms/step - loss: 0.0025
+Epoch 45/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 108ms/step - loss: 0.0025
+Epoch 46/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 103ms/step - loss: 0.0023
+Epoch 47/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 6s 139ms/step - loss: 0.0025
+Epoch 48/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 9s 103ms/step - loss: 0.0022
+Epoch 49/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 6s 146ms/step - loss: 0.0024
+Epoch 50/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 9s 101ms/step - loss: 0.0024
+Epoch 51/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 6s 128ms/step - loss: 0.0023
+Epoch 52/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 101ms/step - loss: 0.0021
+Epoch 53/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 101ms/step - loss: 0.0025
+Epoch 54/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 6s 116ms/step - loss: 0.0024
+Epoch 55/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 102ms/step - loss: 0.0020
+Epoch 56/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 112ms/step - loss: 0.0019
+Epoch 57/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 112ms/step - loss: 0.0020
+Epoch 58/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 102ms/step - loss: 0.0026
+Epoch 59/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 7s 142ms/step - loss: 0.0019
+Epoch 60/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 9s 101ms/step - loss: 0.0021
+Epoch 61/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 6s 133ms/step - loss: 0.0021
+Epoch 62/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 9s 113ms/step - loss: 0.0020
+Epoch 63/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 110ms/step - loss: 0.0021
+Epoch 64/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 100ms/step - loss: 0.0018
+Epoch 65/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 131ms/step - loss: 0.0022
+Epoch 66/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 108ms/step - loss: 0.0020
+Epoch 67/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 101ms/step - loss: 0.0018
+Epoch 68/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 134ms/step - loss: 0.0020
+Epoch 69/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 103ms/step - loss: 0.0016
+Epoch 70/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 103ms/step - loss: 0.0019
+Epoch 71/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 6s 136ms/step - loss: 0.0017
+Epoch 72/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 101ms/step - loss: 0.0017
+Epoch 73/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 102ms/step - loss: 0.0016
+Epoch 74/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 139ms/step - loss: 0.0016
+Epoch 75/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 9s 105ms/step - loss: 0.0018
+Epoch 76/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 6s 120ms/step - loss: 0.0016
+Epoch 77/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 102ms/step - loss: 0.0017
+Epoch 78/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 7s 146ms/step - loss: 0.0016
+Epoch 79/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 9s 110ms/step - loss: 0.0018
+Epoch 80/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 6s 147ms/step - loss: 0.0016
+Epoch 81/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 9s 103ms/step - loss: 0.0015
+Epoch 82/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 7s 145ms/step - loss: 0.0015
+Epoch 83/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 105ms/step - loss: 0.0018
+Epoch 84/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 103ms/step - loss: 0.0018
+Epoch 85/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 6s 132ms/step - loss: 0.0015
+Epoch 86/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 102ms/step - loss: 0.0016
+Epoch 87/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 102ms/step - loss: 0.0015
+Epoch 88/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 6s 122ms/step - loss: 0.0013
+Epoch 89/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 101ms/step - loss: 0.0013
+Epoch 90/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 6s 128ms/step - loss: 0.0016
+Epoch 91/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 103ms/step - loss: 0.0015
+Epoch 92/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 103ms/step - loss: 0.0013
+Epoch 93/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 129ms/step - loss: 0.0013
+Epoch 94/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 111ms/step - loss: 0.0016
+Epoch 95/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 103ms/step - loss: 0.0015
+Epoch 96/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 133ms/step - loss: 0.0015
+Epoch 97/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 4s 106ms/step - loss: 0.0018
+Epoch 98/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 5s 104ms/step - loss: 0.0015
+Epoch 99/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 7s 143ms/step - loss: 0.0017
+Epoch 100/100
+38/38 ━━━━━━━━━━━━━━━━━━━━ 9s 104ms/step - loss: 0.0018
+'''
+
+# **************** Part 3 - Making the predictions and visualising the results ***********
+# Getting the real stock price of 2017
+dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
+real_stock_price = dataset_test.iloc[:, 1:2].values
+
+# Getting the predicted stock price of 2017
+dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis = 0)
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+inputs = inputs.reshape(-1,1)
+inputs = sc.transform(inputs)
+X_test = []
+for i in range(60, 80):
+    X_test.append(inputs[i-60:i, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+# Visualising the results
+plt.plot(real_stock_price, color = 'red', label = 'Real Google Stock Price')
+plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Google Stock Price')
+plt.title('Google Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Google Stock Price')
+plt.legend()
+plt.show()
